2732ac18badc438f48beaab1a8cd749c40cf4ac2
mmt8761 <sourabh.saldi@go-mmt.com>
2021-06-02 20:38:36 +0530

    detect loop

--- src/datastrctures/MyLinkedList.kt
+++ src/datastrctures/MyLinkedList.kt
@@ -1,7 +1,7 @@
 package datastrctures
 
 class MyLinkedList<T>(node: LinkedListNode<T>) {
-     var root = node
+    var root = node
     private var current: LinkedListNode<T> = root
 
     class LinkedListNode<T>(inputData: T) {
@@ -22,15 +22,32 @@ class MyLinkedList<T>(node: LinkedListNode<T>) {
             current = current.next
         }
     }
+
+    fun detectCycle(): Boolean {
+        var slowPointer: LinkedListNode<T>? = root
+        var fastPointer: LinkedListNode<T>? = root.next
+        while (slowPointer != null && fastPointer != null) {
+            if (slowPointer == fastPointer) {
+                return true
+            } else {
+                slowPointer = slowPointer.next
+                fastPointer = fastPointer.next?.next
+            }
+
+        }
+
+        return false
+    }
 }
 
-fun main(){
+fun main() {
     var linkedList = MyLinkedList(MyLinkedList.LinkedListNode(1))
     linkedList.add(2)
     linkedList.add(3)
     linkedList.add(4)
     linkedList.add(5)
     linkedList.add(6)
-    
     linkedList.printLinkedList()
+    linkedList.root.next?.next?.next?.next?.next?.next = linkedList.root.next?.next
+    println(linkedList.detectCycle())
 }
